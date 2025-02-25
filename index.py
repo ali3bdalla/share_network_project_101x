73aa1f7f0ac8e2ddfd2e6ddad7c9484abacd6388
ali abdalla <ali.dev.sd@gmail.com>
2025-02-25 13:26:08 +0100

    locally

--- index.py
+++ index.py
@@ -10,5 +10,5 @@ Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at port {PORT}")
     httpd.serve_forever()
-
+# this comment has been added to the file locally
 print("Contribution from Alex")
